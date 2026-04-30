c2c51516203c6efd6b9d225274fbdf18fe32f5e2
Victor Cadillo <vcadillog@uni.pe>
2026-04-30 11:52:59 -0500

    Agregado ADT de DLListmultinivel

---
+++ PC2/include/MultiLevelDLList.h
@@ -0,0 +1,160 @@
+#pragma once
+#include "DLList.h"
+#include <string>
+#include <unordered_map>
+
+namespace ods {
+
+template <class T> class MultiLevelDLList : public DLList<T> {
+protected:
+  using Node = typename DLList<T>::Node;
+
+  std::unordered_map<Node *, MultiLevelDLList<T> *> children;
+
+  MultiLevelDLList<T> *getChild(Node *u) const {
+    auto it = children.find(u);
+    return (it != children.end()) ? it->second : nullptr;
+  }
+
+public:
+  MultiLevelDLList() : DLList<T>() {}
+
+  MultiLevelDLList<T> *addChild(int i) {
+    Node *u = this->getNode(i);
+
+    if (getChild(u) != nullptr) {
+      return nullptr; 
+    }
+
+    auto *child = new MultiLevelDLList<T>();
+    children[u] = child;
+
+    return child;
+  }
+
+  bool hasChild(int i) { return getChild(this->getNode(i)) != nullptr; }
+
+  void removeNode(Node *w) {
+    MultiLevelDLList<T> *child = getChild(w);
+
+    if (child != nullptr) {
+      delete child;
+      children.erase(w);
+    }
+
+    w->prev->next = w->next;
+    w->next->prev = w->prev;
+    delete w;
+    this->n--;
+  }
+
+  T remove(int i) {
+    Node *w = this->getNode(i);
+    T x = w->x;
+    removeNode(w);
+    return x;
+  }
+
+  int sizeDeep() const {
+    int total = 0;
+
+    Node *u = this->dummy.next;
+    while (u != &this->dummy) {
+      total++;
+
+      MultiLevelDLList<T> *child = getChild(u);
+      if (child != nullptr) {
+        total += child->sizeDeep();
+      }
+
+      u = u->next;
+    }
+
+    return total;
+  }
+
+  void flatten(std::vector<T> &result) const {
+    Node *u = this->dummy.next;
+
+    while (u != &this->dummy) {
+      result.push_back(u->x);
+
+      MultiLevelDLList<T> *child = getChild(u);
+      if (child != nullptr) {
+        child->flatten(result);
+      }
+
+      u = u->next;
+    }
+  }
+
+  std::vector<T> flatten() const {
+    std::vector<T> res;
+    flatten(res);
+    return res;
+  }
+  MultiLevelDLList<T> &buildFromString(const std::vector<std::string> &input) {
+    this->clear();
+    if (input.empty())
+      return *this;
+
+    int i = 0;
+
+    while (i < input.size() && input[i] != "null") {
+      this->add(std::stoi(input[i]));
+      i++;
+    }
+
+    i++; 
+
+    std::vector<std::pair<MultiLevelDLList<T> *, int>> stack;
+
+    for (int j = this->size() - 1; j >= 0; --j) {
+      stack.push_back({this, j});
+    }
+
+    while (!stack.empty() && i < input.size()) {
+
+      auto [list, idx] = stack.back();
+      stack.pop_back();
+
+      if (i >= input.size())
+        break;
+
+      if (input[i] == "null") {
+        i++;
+        continue;
+      }
+
+      auto *child = list->addChild(idx);
+
+      std::vector<int> values;
+
+      while (i < input.size() && input[i] != "null") {
+        values.push_back(std::stoi(input[i]));
+        i++;
+      }
+
+      for (int v : values) {
+        child->add(v);
+      }
+
+      for (int j = child->size() - 1; j >= 0; --j) {
+        stack.push_back({child, j});
+      }
+
+      if (i < input.size())
+        i++; 
+    }
+
+    return *this;
+  }
+
+  ~MultiLevelDLList() {
+    for (auto &p : children) {
+      delete p.second;
+    }
+  }
+};
+
+} // namespace ods
