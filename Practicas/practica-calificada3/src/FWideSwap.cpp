3bae00f5083dcadc93f0a33e0733e5825673d757
Victor Cadillo <vcadillog@uni.pe>
2026-06-10 22:06:12 -0500

    optimizado swap

--- Practicas/practica-calificada3/src/FWideSwap.cpp
+++ Practicas/practica-calificada3/src/FWideSwap.cpp
@@ -14,46 +14,44 @@ void FWideSwap::swapPositions(int i, int j) {
   if (i == j) {
     return;
   }
-
-  std::vector<int> arr = toVector();
-
-  std::swap(arr[i], arr[j]);
-
-  clear();
-
-  for (int x : arr) {
-    pushBack(x);
+  int vi = getAt(i);
+  int vj = getAt(j);
+  if (i > j) {
+    std::swap(i, j);
+    std::swap(vi, vj);
   }
+  eraseAt(j);
+  eraseAt(i);
+
+  insertAt(i, vj);
+  insertAt(j, vi);
 }
 void FWideSwap::solve(int K) {
-  bool changed = true;
+  int n = size();
 
+  std::vector<int> pos(n + 2, 0);
+
+  {
+    auto p = toVector();
+    for (int i = 0; i < n; i++) {
+      pos[p[i]] = i;
+    }
+  }
+
+  bool changed = true;
   while (changed) {
     changed = false;
 
-    auto p = toVector();
-
-    int n = static_cast<int>(p.size());
-
     for (int a = 1; a < n; a++) {
-      int posA = -1;
-      int posB = -1;
-
-      for (int i = 0; i < n; i++) {
-        if (p[i] == a) {
-          posA = i;
-        }
-
-        if (p[i] == a + 1) {
-          posB = i;
-        }
-      }
-
-      if (posA > posB && std::abs(posA - posB) >= K) {
+      int posA = pos[a];
+      int posB = pos[a + 1];
+      if (posA > posB && (posA - posB) >= K) {
         swapPositions(posA, posB);
 
-        changed = true;
+        pos[a] = posB;
+        pos[a + 1] = posA;
 
+        changed = true;
         break;
       }
     }
