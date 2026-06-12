0c25f70f3ffa9fb90ca070b8e95392a47d38fd49
Victor Cadillo <vcadillog@uni.pe>
2026-06-11 23:21:26 -0500

    refactor toVector

--- Practicas/practica-calificada3/include/FWideSwap.h
+++ Practicas/practica-calificada3/include/FWideSwap.h
@@ -1,17 +1,15 @@
 #pragma once
 #include "ImplicitTreap.h"
+#include <vector>
 
 namespace ods {
 
 class FWideSwap : public ImplicitTreap<int> {
 public:
   FWideSwap() = default;
-
   void loadPermutation(const std::vector<int> &p);
-
   void swapPositions(int i, int j);
-
   void solve(int K);
+  std::vector<int> FWStoVector(std::vector<int> p, int K);
 };
-
 } // namespace ods
